53a59af5da59116913683e4aa77945608f95f764
Luis Narquel <luisnarquel@MacBook-Air-de-Luis.local>
2025-12-22 16:30:47 +0000

    update

--- 5_sddm.sh
+++ 5_sddm.sh
@@ -15,7 +15,6 @@ sudo pacman -Syu --noconfirm
 log "Installing SDDM"
 sudo pacman -S --needed --noconfirm sddm
 sudo pacman -S --needed --noconfirm qt5-graphicaleffects qt5-quickcontrols2
-sudo pacman -S --needed --noconfirm qt6-svg qt6-virtualkeyboard qt6-multimedia-ffmpeg
 
 # ===== Create Hyprland UWSM session file =====
 log "Creating Hyprland UWSM session file"
@@ -27,31 +26,8 @@ Name=Hyprland (UWSM)
 Comment=Hyprland started via UWSM
 Exec=uwsm start hyprland
 Type=Application
- DesktopNames=Hyprland
+DesktopNames=Hyprland
 EOF
-
-# ===== Install SDDM Silvia Theme =====
-log "Installing SDDM Silvia theme"
-SCRIPT_DIR="$(dirname "$0")"
-SDDM_CONFIG_PATH="$SCRIPT_DIR/config/sddm"
-THEME_NAME="silvia"
-THEME_PATH="/usr/share/sddm/themes/$THEME_NAME"
-
-if [ ! -d "$SDDM_CONFIG_PATH" ]; then
-	warn "SDDM configuration directory not found at $SDDM_CONFIG_PATH"
-	warn "Skipping theme installation"
-else
-	log "Copying theme files to $THEME_PATH"
-	sudo mkdir -p "$THEME_PATH"
-	sudo cp -rf "$SDDM_CONFIG_PATH"/* "$THEME_PATH"/
-
-	log "Copying theme fonts to /usr/share/fonts"
-	sudo mkdir -p /usr/share/fonts
-	sudo cp -r "$THEME_PATH"/fonts/{redhat,redhat-vf} /usr/share/fonts/
-
-	log "Updating metadata.desktop to use Silvia configuration"
-	sudo sed -i 's/^ConfigFile=configs\/default\.conf$/ConfigFile=configs\/silvia.conf/' "$THEME_PATH"/metadata.desktop
-fi
 
 # ===== Configure SDDM =====
 log "Configuring SDDM"
@@ -60,15 +36,13 @@ sudo mkdir -p /etc/sddm.conf.d
 cat <<EOF | sudo tee /etc/sddm.conf.d/autologin.conf
 [General]
 DisplayServer=wayland
-InputMethod=qtvirtualkeyboard
-GreeterEnvironment=QML2_IMPORT_PATH=/usr/share/sddm/themes/$THEME_NAME/components/,QT_IM_MODULE=qtvirtualkeyboard
 
 [Autologin]
 User=$USER
 Session=hyprland-uwsm
 
 [Theme]
-Current=$THEME_NAME
+Current=breeze
 EOF
 
 # ===== Enable SDDM service =====
